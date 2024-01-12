0c07163b996cd7c3800d8d7279458737897cb5bb
Chris Jeon <chris@typefast.co>
2024-01-12 09:55:49 -0500

    Add products and add some tests

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,7 +1,10 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Homes", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "succeeds" do
+      get root_path
+      expect(response).to have_http_status(:success)
+    end
   end
 end
