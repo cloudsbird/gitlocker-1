9c46d9cfedd02e43950a943d24613f50f72b8f32
Chris Jeon <chris@typefast.co>
2024-01-17 17:54:02 -0500

    Update tests

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    # redirect_to complete_registrations_path if current_user.registration_pending?
+    redirect_to complete_registrations_path if current_user.registration_pending?
   end
 end
--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -3,12 +3,22 @@ require "rails_helper"
 RSpec.describe "Dashboards", type: :request do
   let(:user) { create(:user) }
 
-  before { sign_in user }
-
   describe "GET /index" do
-    it "succeeds" do
-      get dashboard_path
-      expect(response).to have_http_status(:success)
+    context "when user has not finished registration" do
+      it "redirects to complete_registrations_path" do
+        sign_in user
+        get dashboard_path
+        expect(response).to redirect_to complete_registrations_path
+      end
+    end
+
+    context "when user has completed registration" do
+      it "succeeds" do
+        user.registration_completed!
+        sign_in user
+        get dashboard_path
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end
