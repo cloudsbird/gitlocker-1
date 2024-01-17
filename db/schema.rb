30695cb3d49733151449aeecde958c297d500bda
Chris Jeon <chris@typefast.co>
2024-01-17 09:32:12 -0500

    Add price to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_17_141153) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_17_143011) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_17_141153) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price_cents", default: 0, null: false
+    t.string "price_currency", default: "USD", null: false
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
