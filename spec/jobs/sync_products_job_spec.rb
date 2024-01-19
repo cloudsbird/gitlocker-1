9b0286d3eb8f7c489128455a91cf6ac40bd2443b
Chris Jeon <chris@typefast.co>
2024-01-19 13:04:55 -0500

    Add tests for SyncProductsJob

--- spec/jobs/sync_products_job_spec.rb
+++ spec/jobs/sync_products_job_spec.rb
@@ -1,5 +1,18 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe SyncProductsJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user, token: SecureRandom.urlsafe_base64) }
+
+  before do
+    allow_any_instance_of(described_class).to receive(:repositories).and_return(
+      [
+        double(:repository, name: "product1", description: "123", html_url: "https://www.github.com/1"),
+        double(:repository, name: "product2", description: "123", html_url: "https://www.github.com/2")
+      ]
+    )
+  end
+
+  it "imports 2 products" do
+    expect { described_class.perform_now(user.id) }.to change { Product.count }.from(0).to(2)
+  end
 end
