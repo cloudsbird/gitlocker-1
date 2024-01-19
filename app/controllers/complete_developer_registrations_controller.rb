b481c80b9fc823fed11fd6326e7485356ec34820
Chris Jeon <chris@typefast.co>
2024-01-19 15:19:47 -0500

    Add tests for complete developer registrations

--- app/controllers/complete_developer_registrations_controller.rb
+++ app/controllers/complete_developer_registrations_controller.rb
@@ -1,6 +1,17 @@
 class CompleteDeveloperRegistrationsController < ApplicationController
   before_action :authenticate_user!
+  before_action :redirect_to_dashboard, if: -> { current_user.registration_completed? }
 
   def index
+    SyncProductsJob.perform_later(current_user.id) unless current_user.synced?
+  end
+
+  def update
+  end
+
+  private
+
+  def redirect_to_dashboard
+    redirect_to dashboard_path
   end
 end
--- spec/requests/complete_developer_registrations_spec.rb
+++ spec/requests/complete_developer_registrations_spec.rb
@@ -5,10 +5,23 @@ RSpec.describe "CompleteDeveloperRegistrations", type: :request do
 
   before { sign_in user }
 
-  pending do
-    describe "GET index" do
+  describe "GET index" do
+    before do
+      allow(SyncProductsJob).to receive(:perform_later).and_return(true)
     end
 
+    it "queues up SyncProductsJob" do
+      expect(SyncProductsJob).to receive(:perform_later).with(user.id)
+      get complete_developer_registrations_path
+    end
+
+    it "succeeds" do
+      get complete_developer_registrations_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  pending do
     describe "PUT update" do
     end
   end
