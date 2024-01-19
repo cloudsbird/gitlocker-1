8af24e0f8b7da780e60e6322b361c3b87269b1ab
Chris Jeon <chris@typefast.co>
2024-01-19 14:52:40 -0500

    spec for put update

--- spec/requests/complete_registrations_spec.rb
+++ spec/requests/complete_registrations_spec.rb
@@ -22,8 +22,40 @@ RSpec.describe "CompleteRegistrations", type: :request do
     end
   end
 
-  pending do
-    describe "PUT update" do
+  describe "PUT update" do
+    let(:params) {}
+    let(:request) do
+      put complete_registration_path, params: params
+    end
+
+    context "when user_type is customer" do
+      let(:params) { { user_type: "customer"} }
+
+      it "completes user registration" do
+        expect do
+          request
+        end.to change { user.registration_completed? }.from(false).to(true)
+      end
+
+      it "redirects to dashboard_path" do
+        request
+        expect(response).to redirect_to dashboard_path
+      end
+    end
+
+    context "when user_type is developer" do
+      let(:params) { { user_type: "developer" } }
+
+      it "does not complete user registration" do
+        expect do
+          request
+        end.not_to change { user.registration_completed? }
+      end
+
+      it "redirects to complete_developer_registrations_pathjla" do
+        request
+        expect(response).to redirect_to complete_developer_registrations_path
+      end
     end
   end
 end
