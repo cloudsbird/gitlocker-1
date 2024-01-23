04c90a97df94601d9a846c27addeb434a478a00c
Chris Jeon <chris@typefast.co>
2024-01-23 11:09:40 -0500

    JS for synchronizing products

--- app/javascript/controllers/github_sync_controller.js
+++ app/javascript/controllers/github_sync_controller.js
@@ -4,16 +4,23 @@ import { get } from '@rails/request.js';
 // Connects to data-controller="github-sync"
 export default class extends Controller {
   connect() {
-    console.log('apiUrl: ', this.element.dataset.apiUrl);
+    console.log('syncApiUrl: ', this.element.dataset.syncApiUrl);
+    console.log('productApiUrl: ', this.element.dataset.productApiUrl);
     this.checkSync();
   }
 
   async renderProducts() {
     console.log('inside renderAllProducts');
+    const response = await get(this.element.dataset.productApiUrl, { responseKind: 'json' });
+
+    if (response.ok) {
+      const responseBody = await response.json
+      console.log('products responseBody: ', responseBody);
+    }
   }
 
   async checkSync() {
-    const response = await get(this.element.dataset.apiUrl, { responseKind: 'json' });
+    const response = await get(this.element.dataset.syncApiUrl, { responseKind: 'json' });
 
     if (response.ok) {
       const responseBody = await response.json
