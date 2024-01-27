074710986c7830523ab390ddd2792213857aaf7c
Chris Jeon <chris@typefast.co>
2024-01-27 17:16:17 -0500

    Hover effect on menu items

--- app/javascript/controllers/product_card_controller.js
+++ app/javascript/controllers/product_card_controller.js
@@ -1,11 +1,26 @@
 import { Controller } from '@hotwired/stimulus';
 import { toggle } from 'el-transition';
+import { useHover } from 'stimulus-use';
 
 // Connects to data-controller="product-card"
 export default class extends Controller {
-  static targets = ['menu'];
+  static targets = ['menu', 'menuItem'];
+
+  connect() {
+    this.menuItemTargets.forEach((menuItem) => {
+      useHover(this, { element: menuItem });
+    });
+  }
 
   open() {
     toggle(this.menuTarget);
+  }
+
+  mouseEnter(e) {
+    e.target.classList.add('bg-gray-50');
+  }
+
+  mouseLeave(e) {
+    e.target.classList.remove('bg-gray-50');
   }
 }
