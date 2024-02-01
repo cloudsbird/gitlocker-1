1701834eb25025b0a6c0001deb19ec4f8fdc8be4
Chris Jeon <chris@typefast.co>
2024-01-31 21:42:10 -0500

    Try uploading images without major JS interactions

--- app/javascript/controllers/product_form_controller.js
+++ app/javascript/controllers/product_form_controller.js
@@ -7,29 +7,29 @@ const imageTypes = ['image/gif', 'image/jpeg', 'image/png'];
 export default class extends Controller {
   static targets = ['coverInput'];
 
-  attachCover(e) {
-    e.preventDefault();
-
-    const file = e.target.files[0];
-
-    if (!imageTypes.includes(file.type)) {
-      alert('Attached file must be an image');
-    }
-
-    const image = new Image();
-    const imageObject = URL.createObjectURL(file);
-
-    image.addEventListener('load', () => {
-      if (image.width < 1280 || image.height < 720) {
-        alert('Cover Image must be at least 1280x720px');
-        return;
-      }
-
-      console.log('coverInputTarget.files: ', this.coverInputTarget.files);
-      this.coverInputTarget.files.push(e.target.files);
-      this.coverTarget.src = imageObject;
-    });
-
-    image.src = imageObject;
-  }
+  // attachCover(e) {
+  //   e.preventDefault();
+  //
+  //   const file = e.target.files[0];
+  //
+  //   if (!imageTypes.includes(file.type)) {
+  //     alert('Attached file must be an image');
+  //   }
+  //
+  //   const image = new Image();
+  //   const imageObject = URL.createObjectURL(file);
+  //
+  //   image.addEventListener('load', () => {
+  //     if (image.width < 1280 || image.height < 720) {
+  //       alert('Cover Image must be at least 1280x720px');
+  //       return;
+  //     }
+  //
+  //     console.log('coverInputTarget.files: ', this.coverInputTarget.files);
+  //     this.coverInputTarget.files = e.target.files;
+  //     this.coverTarget.src = imageObject;
+  //   });
+  //
+  //   image.src = imageObject;
+  // }
 }
