0a479d902f104c4fbe656c1cf32887bd5ca94f93
Chris Jeon <chris@typefast.co>
2024-02-01 18:12:59 -0500

    Home page modal content

--- app/components/dialog_component.rb
+++ app/components/dialog_component.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class DialogComponent < ViewComponent::Base
-  def initialize(size: "sm", icon: nil, headline: nil, body: nil, cancel_button: nil, confirm_button: nil)
+  def initialize(
+    size: "sm",
+    icon: nil,
+    headline: nil,
+    body: nil,
+    cancel_button: nil,
+    confirm_button: nil
+  )
     @modal_size = "max-w-#{size}"
     @headline = headline
     @body = body
