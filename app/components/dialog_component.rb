8d0d45dd099d9a5eaed7374c540db73ec30e1bf4
Chris Jeon <chris@typefast.co>
2024-02-01 11:04:45 -0500

    Create DialogComponent

---
+++ app/components/dialog_component.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class DialogComponent < ViewComponent::Base
+  def initialize(size: "sm", icon: nil, headline: nil, body: nil, cancel_button: nil, confirm_button: nil)
+    @modal_size = "max-w-#{size}"
+    @headline = headline
+    @body = body
+    @cancel_button = cancel_button
+    @confirm_button = confirm_button
+  end
+end
