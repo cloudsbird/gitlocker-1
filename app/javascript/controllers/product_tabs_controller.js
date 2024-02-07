09b14401ae43fd362106595f87127b1f5b0a8b83
Chris Jeon <chris@typefast.co>
2024-02-07 15:19:16 -0500

    Switching tabs throughout show page

--- app/javascript/controllers/product_tabs_controller.js
+++ app/javascript/controllers/product_tabs_controller.js
@@ -1,16 +1,33 @@
 import { Controller } from '@hotwired/stimulus';
 
+const buttonClasses = {
+  'active': ['border-indigo-600', 'text-indigo-600'],
+  'inactive': ['border-transparent', 'text-gray-700', 'hover:border-gray-300', 'hover:text-gray-800']
+};
+
 // Connects to data-controller="product-tabs"
 export default class extends Controller {
-  static targets = ['button'];
-  connect() {
-    console.log('hello world product tabs');
-  }
+  static targets = ['button', 'panel'];
 
   toggleTab(e) {
     const tabName = e.target.id.replace('tab-', '');
-    console.log('inside toggleTab: ', tabName);
 
-    document.getElementById(`tab-panel-${tabName}`);
+    this.buttonTargets.forEach((button) => {
+      if (button.id === e.target.id) {
+        button.classList.add(...buttonClasses['active']);
+        button.classList.remove(...buttonClasses['inactive']);
+      } else {
+        button.classList.add(...buttonClasses['inactive']);
+        button.classList.remove(...buttonClasses['active']);
+      }
+    });
+
+    this.panelTargets.forEach((panel) => {
+      if (panel.id === `panel-${tabName}`) {
+        panel.classList.remove('hidden');
+      } else {
+        panel.classList.add('hidden');
+      }
+    });
   }
 }
