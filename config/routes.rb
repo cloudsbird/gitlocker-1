2f024d722e0de5b4173feb1529e588696bbae279
Chris Jeon <chris@typefast.co>
2024-02-13 08:09:23 -0500

    Comment out fog required stuff

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  get "/sitemap.xml", to: redirect("https://s3.amazonaws.com/gitlocker/sitemaps/sitemap.xml", status: 301)
+
   # Defines the root path route ("/")
   root "home#index"
 
