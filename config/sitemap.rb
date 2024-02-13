b849875bc572f5d62801906032337b58bd09e3ba
Chris Jeon <chris@typefast.co>
2024-02-13 08:57:13 -0500

    Re-enable sitemap AWS config

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,17 +1,17 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://www.gitlocker.com"
 
-# SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(
-#   fog_provider: "AWS",
-#   aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-#   aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-#   fog_directory: ENV["S3_BUCKET"],
-#   fog_region: ENV["AWS_REGION"]
-# )
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(
+  fog_provider: "AWS",
+  aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+  aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+  fog_directory: ENV["S3_BUCKET"],
+  fog_region: ENV["AWS_REGION"]
+)
 
 SitemapGenerator::Sitemap.public_path = "tmp/"
 
-# SitemapGenerator::Sitemap.sitemaps_host = "https://#{ENV['S3_BUCKET']}.s3.amazonaws.com/"
+SitemapGenerator::Sitemap.sitemaps_host = "https://#{ENV['S3_BUCKET']}.s3.amazonaws.com/"
 
 SitemapGenerator::Sitemap.sitemaps_path = "sitemaps/"
 
