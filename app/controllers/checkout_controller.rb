4caf28a4e385f60af77f52b1aa09d4915cc632ce
Chris Jeon <chris@typefast.co>
2024-02-20 11:48:55 -0500

    Cart item inc heckout page in partials

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -2,6 +2,6 @@ class CheckoutController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @cart_items = current_user.cart_items
+    @cart_items = current_user.cart_items.includes(:product)
   end
 end
