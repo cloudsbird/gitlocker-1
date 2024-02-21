ea9b78b5a91fb596ecb2a955c8c32af196a1cee5
Chris Jeon <chris@typefast.co>
2024-02-21 10:07:59 -0500

    Ability to update profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     authorize(@user)
   end
+
+  def update
+    @user = User.find(params[:id])
+    authorize(@user)
+    @user.update(user_params)
+    redirect_to edit_user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :name, :profile_picture)
+  end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "Users", type: :request do
-  let(:user) { create(:user) }
+  let(:user) { create(:user, email: "user@gitlocker.com") }
 
   before { sign_in user }
 
@@ -21,6 +21,38 @@ RSpec.describe "Users", type: :request do
         expect do
           get edit_user_path(user)
         end.to raise_error(Pundit::NotAuthorizedError)
+      end
+    end
+  end
+
+  describe "PUT update" do
+    let(:update_request) do
+      put user_path(user), params: {
+        user: {
+          email: "foo@bar.com"
+        }
+      }
+    end
+
+    context "when invalid user" do
+      it "raises pundit error" do
+        other_user = create(:user)
+        sign_out user
+        sign_in other_user
+        expect { update_request }.to raise_error(Pundit::NotAuthorizedError)
+      end
+    end
+
+    context "when valid user" do
+      it "successfully updates user profile" do
+        expect do
+          update_request
+        end.to change { user.reload.email }.from("user@gitlocker.com").to("foo@bar.com")
+      end
+
+      it "redirects to edit_user_path" do
+        update_request
+        expect(response).to redirect_to(edit_user_path(user))
       end
     end
   end
