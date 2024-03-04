607580a8168e4206806b4c8ea4d08ed7fa20713e
Chris Jeon <chris@typefast.co>
2024-03-04 07:40:35 -0500

    Code for paying for stuff

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -17,8 +17,32 @@ class CheckoutController < ApplicationController
       end
       Purchase.import(purchases, on_duplicate_key_ignore: true)
       current_user.reload.cart_items.destroy_all
+      # pay_for_stuff
     end
 
     redirect_to root_path
+  end
+
+  private
+
+  def pay_for_stuff
+    stripe_customer ||= if current_user.stripe_id.blank?
+                          customer = Stripe::Customer.create(email: current_user.email)
+                          current_user.update(stripe_id: customer.id)
+                          customer
+                        else
+                          Stripe::Customer.retrieve(current_user.stripe_id)
+                        end
+
+    stripe_card = Stripe::Customer.create_source(
+      stripe_customer.id,
+      { source: params[:stripeToken] }
+    )
+    charge = Stripe::Charge.create(
+      amount: Money.from_amount(BigDecimal(params[:total])).cents,
+      currency: "usd",
+      source: stripe_card.id,
+      customer: stripe_customer.id
+    )
   end
 end
