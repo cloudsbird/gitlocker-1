91fae4078d488172dee489c6e8e5777190671895
Chris Jeon <chris@typefast.co>
2024-04-14 15:23:05 -0500

    Add names and image name list

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,5 +1,23 @@
 class Language < ApplicationRecord
   has_many :products
 
+  NAMES = {
+    "C#": "c-sharp.png",
+    "C": "c.png",
+    "C++": "c_plus_plus.png",
+    "CSS": "css.png",
+    "GoLang": "golang.png",
+    "HTML": "html.png",
+    "Java": "java.png",
+    "JavaScript": "javascript.png",
+    "Kotlin": "kotlin.png",
+    "PHP": "php.png",
+    "Python": "python2.jpg",
+    "Ruby": "ruby.png",
+    "Rust": "rust.png",
+    "SQL": "sql.png",
+    "Swift": "swift.png"
+  }.with_indifferent_access
+
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
