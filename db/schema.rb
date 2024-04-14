245fe5d8eaa1399ada8f4713aa1e29bfcfcc22c3
Chris Jeon <chris@typefast.co>
2024-04-14 15:46:42 -0500

    Create pg_search documents table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_14_204100) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_14_204610) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -150,6 +150,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_14_204100) do
     t.string "stripe_charge_id"
     t.boolean "commission_paid", default: false, null: false
     t.index ["user_id"], name: "index_payments_on_user_id"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
   end
 
   create_table "products", force: :cascade do |t|
