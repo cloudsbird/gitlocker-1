f41997093df4a768f15ec53760fd7ad960b22662
Chris Jeon <chris@typefast.co>
2024-04-17 13:22:47 -0500

    Migrations to add unique index to categories.name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_17_181626) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_17_182144) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,9 +55,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_17_181626) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "good_job_batches", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
