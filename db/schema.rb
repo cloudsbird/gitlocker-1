7ea12b2824e2b2f3bc16c0b99b312d8bfb9b7fa5
Chris Jeon <chris@typefast.co>
2024-04-17 13:50:27 -0500

    Add product_id category_id unique index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_17_182712) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_17_184932) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -174,6 +174,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_17_182712) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_product_categories_on_category_id"
+    t.index ["product_id", "category_id"], name: "index_product_categories_on_product_id_and_category_id", unique: true
     t.index ["product_id"], name: "index_product_categories_on_product_id"
   end
 
