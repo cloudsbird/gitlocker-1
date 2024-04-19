f04e93f29292107015983d2455bb59351fce4e1a
Chris Jeon <chris@typefast.co>
2024-04-18 20:29:16 -0500

    Add featured to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_17_185307) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_19_012850) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -195,6 +195,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_17_185307) do
     t.integer "reviews_count", default: 0, null: false
     t.float "average_rating", default: 0.0, null: false
     t.bigint "language_id", null: false
+    t.boolean "featured", default: false, null: false
     t.index ["language_id"], name: "index_products_on_language_id"
     t.index ["repo_id"], name: "index_products_on_repo_id", unique: true
     t.index ["slug"], name: "index_products_on_slug", unique: true
