e2ae28215e7b75dae236d15d0964db1245f4cf15
devhassanmahmood <devhassan.mahmood@gmail.com>
2024-05-06 21:10:30 +0500

    Show email info

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,9 @@
 module Users
   class SessionsController < Devise::SessionsController
     include DeviseCallbacks
+    def new
+      @github_email = session[:github_email]
+     puts "GitHub email retrieved from session: #{@github_email}"
+    end
   end
 end
