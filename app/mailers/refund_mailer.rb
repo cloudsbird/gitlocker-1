bc3fd7315754916ed4992a3d56726fb92672b508
Hassan Mahmood <devhassan.mahmood@gmail.com>
2024-05-27 17:48:16 +0500

    Update email

--- app/mailers/refund_mailer.rb
+++ app/mailers/refund_mailer.rb
@@ -2,6 +2,6 @@ class RefundMailer < ApplicationMailer
   def notify_admin(refund, user)
     @refund = refund
     @user = user
-    mail(to: 'cody@gitlocker.com', subject: 'New Refund Request', from: @user.email)
+    mail(to: 'cody@gitlocker.com', subject: 'New Refund Request', from: 'support@gitlocker.com')
   end
 end
