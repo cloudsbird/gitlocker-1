b45b9bd67d54c11f2f1d4855b6942bb0e99a1f4f
Hassan Mahmood <devhassan.mahmood@gmail.com>
2024-05-29 17:35:02 +0500

    Updated funds

--- app/controllers/funds_controller.rb
+++ app/controllers/funds_controller.rb
@@ -6,25 +6,29 @@ class FundsController < ApplicationController
   def create
     seller = current_user
     balance_in_cents = (current_user.balance * 100).to_i
-    amount = [balance_in_cents, 50].max
+    amount = [balance_in_cents, 0].max
     stripe_token = params[:stripeToken]
-
     begin
+      if( amount > 0)
 
-      transfer = Stripe::Transfer.create({
-        amount: amount,
-        currency: 'usd',
-        destination: seller.account.stripe_id,
-        transfer_group: 'ORDER_95',
-      })
-      transfer_from_api = Stripe::Transfer.retrieve(transfer.id)
+        transfer = Stripe::Transfer.create({
+          amount: amount,
+          currency: 'usd',
+          destination: seller.account.stripe_id,
+          transfer_group: 'ORDER_95',
+        })
+        transfer_from_api = Stripe::Transfer.retrieve(transfer.id)
 
-      if transfer_from_api.reversed
-        redirect_to funds_path, alert: 'Payment processing failed!'
-        puts "Transfer has been reversed"
+        if transfer_from_api.reversed
+          redirect_to funds_path, alert: 'Payment processing failed!'
+          puts "Transfer has been reversed"
+        else
+          seller.update(balance: 0.00)
+          redirect_to funds_path, notice: 'Payment successful!'
+        end
       else
-        seller.update(balance: 0.00)
-        redirect_to funds_path, notice: 'Payment successful!'
+        Rails.logger.error "Balance is Nil"
+        redirect_to funds_path, alert: 'Balance is Nil'
       end
     rescue Stripe::InvalidRequestError, Stripe::CardError, Stripe::AuthenticationError, Stripe::APIConnectionError, Stripe::StripeError => e
       Rails.logger.error "Stripe Error: #{e.message}"
