8b1cc343d00c73e6335b78ebf0598c5bb23d32e5
Hassan Mahmood <devhassan.mahmood@gmail.com>
2024-06-25 16:30:29 +0500

    Remove debugger

--- app/javascript/controllers/search_link_controller.js
+++ app/javascript/controllers/search_link_controller.js
@@ -3,7 +3,6 @@ import { Controller } from '@hotwired/stimulus';
 // Connects to data-controller="search-link"
 export default class extends Controller {
   openDialog(e) {
-    debugger
     e.preventDefault();
 
     document.getElementById(this.element.dataset.searchDialogId).classList.remove('hidden');
