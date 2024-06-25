5d28e83068f7d52f1355b06e30339d3010aaa379
Hassan Mahmood <devhassan.mahmood@gmail.com>
2024-06-25 16:26:02 +0500

    Updated Mobile Responsiveness

--- app/javascript/controllers/search_link_controller.js
+++ app/javascript/controllers/search_link_controller.js
@@ -3,6 +3,7 @@ import { Controller } from '@hotwired/stimulus';
 // Connects to data-controller="search-link"
 export default class extends Controller {
   openDialog(e) {
+    debugger
     e.preventDefault();
 
     document.getElementById(this.element.dataset.searchDialogId).classList.remove('hidden');
