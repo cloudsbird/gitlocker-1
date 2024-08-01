3e77bb86a83fd8dace448350d8526e9e4b21a76f
Hassan Mahmood <devhassan.mahmood@gmail.com>
2024-08-01 20:55:38 +0500

    language tags for products && fixed search for selected category

--- app/controllers/marketplace/search_results_controller.rb
+++ app/controllers/marketplace/search_results_controller.rb
@@ -1,16 +1,22 @@
 module Marketplace
-class SearchResultsController < ApplicationController
-  def index
-    if params[:search].present?
-      # results = PgSearch.multisearch(params[:search])
-      # @products = results.map(&:searchable)
-      @product_results = Product.search(params[:search])
-      @marketplace_creator_results = User.search(params[:search])
-      @category_results = Category.search(params[:search])
-      @language_results = Language.search(params[:search])
-    else
-      @products = []
+  class SearchResultsController < ApplicationController
+    def index
+      if params[:search].present?
+        @product_results = Product.search(params[:search])
+        @marketplace_creator_results = User.search(params[:search])
+        @category_results = Category.search(params[:search])
+        @language_results = Language.search(params[:search])
+
+        if params[:category_id].present?
+          @product_results = @product_results.joins(:product_categories)
+                                             .where(product_categories: { category_id: params[:category_id] })
+        end
+      else
+        @product_results = []
+        @marketplace_creator_results = []
+        @category_results = []
+        @language_results = []
+      end
     end
   end
-end
 end
