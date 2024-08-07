466c93d87d623615c26bf953a2fb2831b70a5a4f
Hassan Mahmood <devhassan.mahmood@gmail.com>
2024-08-07 21:35:53 +0500

    2500 words validation for blog content

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,13 @@
 class Blog < ApplicationRecord
   has_one_attached :image
 
-  validates :content, length: { maximum: 20000, message: "Content should not exceed 20,000 characters" }
+  validate :content_word_count
+
+  def content_word_count
+    if content.present? && content.split.size > 2500
+      errors.add(:content, "should not exceed 2500 words")
+    end
+  end
 
   def self.ransackable_attributes(auth_object = nil)
     ["title", "content", "created_at", "updated_at"]
