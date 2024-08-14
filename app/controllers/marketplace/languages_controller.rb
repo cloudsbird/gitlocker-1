1b902414a411c6ab9ea604edb430f07cf6078431
Cody Rutscher <44309879+codyrutscher@users.noreply.github.com>
2024-08-14 04:01:13 -0500

    Update languages_controller.rb

--- app/controllers/marketplace/languages_controller.rb
+++ app/controllers/marketplace/languages_controller.rb
@@ -2,7 +2,7 @@
 module Marketplace
 class LanguagesController < ApplicationController
   def show
-    @language = Language.friendly.find(params[:slug])
+    @language = Language.friendly.find(params[:id])
   end
 end
 end
