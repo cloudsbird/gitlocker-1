4ca86c6aedf6f83249d122c18c3b3298fbc1127a
bablu kumar <bk147411@gmail.com>
2024-08-25 04:01:49 +0530

    fixed sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,18 +1,24 @@
 # config/initializers/sidekiq.rb
-# if Rails.env.development?
-#   Sidekiq.configure_server do |config|
-#     config.redis = { url: 'redis://localhost:6379/0' }
-#   end
-
-#   Sidekiq.configure_client do |config|
-#     config.redis = { url: 'redis://localhost:6379/0' }
-#   end
-# else
+if Rails.env.development?
   Sidekiq.configure_server do |config|
     config.redis = { url: 'redis://localhost:6379/0' }
   end
-
+  
   Sidekiq.configure_client do |config|
     config.redis = { url: 'redis://localhost:6379/0' }
   end
-# end
+else
+   Sidekiq.configure_server do |config|
+    config.redis = {
+      url: ENV["REDIS_URL"],
+      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+    }
+
+  Sidekiq.configure_client do |config|
+    config.redis = {
+      url: ENV["REDIS_URL"],
+      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+    }
+
+  end
+end
