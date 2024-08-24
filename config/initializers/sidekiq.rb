752aa88c7ac64c98540c46cabae1fc0bfcc4a814
bablu kumar <bk147411@gmail.com>
2024-08-25 03:54:11 +0530

    fixed sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,13 @@
 # config/initializers/sidekiq.rb
-if Rails.env.development?
+# if Rails.env.development?
+#   Sidekiq.configure_server do |config|
+#     config.redis = { url: 'redis://localhost:6379/0' }
+#   end
+
+#   Sidekiq.configure_client do |config|
+#     config.redis = { url: 'redis://localhost:6379/0' }
+#   end
+# else
   Sidekiq.configure_server do |config|
     config.redis = { url: 'redis://localhost:6379/0' }
   end
@@ -7,12 +15,4 @@ if Rails.env.development?
   Sidekiq.configure_client do |config|
     config.redis = { url: 'redis://localhost:6379/0' }
   end
-else
-  Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://localhost:6379/0' }
-  end
-
-  Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://localhost:6379/0' }
-  end
-end
+# end
