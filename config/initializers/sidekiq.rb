09baa5043098a094fa65c72e646ecd251ef69259
bablu kumar <bk147411@gmail.com>
2024-08-25 01:43:21 +0530

    added redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 # config/initializers/sidekiq.rb
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: 'rediss://:pc9032c51251fb0fff9eb30af0e91c334f81d1d350055aeb44e30474385d77c98@ec2-54-161-174-175.compute-1.amazonaws.com:27840' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: 'rediss://:pc9032c51251fb0fff9eb30af0e91c334f81d1d350055aeb44e30474385d77c98@ec2-54-161-174-175.compute-1.amazonaws.com:27840' }
 end
