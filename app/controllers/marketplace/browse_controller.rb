aaaa4a4bed647a2387dd566afdc46a6720e8b62d
Hassan Mahmood <devhassan.mahmood@gmail.com>
2024-08-26 20:07:48 +0500

    filters added and sorting on pagination fixed

--- app/controllers/marketplace/browse_controller.rb
+++ app/controllers/marketplace/browse_controller.rb
@@ -1,38 +1,55 @@
 module Marketplace
-class BrowseController < ApplicationController
-  def index
-    @products = Product.exclude_purchased(current_user).filter_and_sort(filter_params).page(params[:page]).per(50)
-  end
-
-  def popular
-    @products = Product.exclude_purchased(current_user).order(purchases_count: :desc).filter_and_sort(filter_params).page(params[:page]).per(50)
-  end
-
-  def recent
-    @products = Product.exclude_purchased(current_user)
-                       .filter_and_sort(filter_params)
-                       .page(params[:page]).per(50)
-
-    respond_to do |format|
-      format.html
-      format.js
+  class BrowseController < ApplicationController
+    def index
+      @products = apply_filters_and_sort(Product.exclude_purchased(current_user))
     end
-  end
 
-  def languages
-    @languages = sort_categories(Language.page(params[:page]).per(50), params[:sort_by])
-  end
+    def popular
+      @products = apply_filters_and_sort(Product.exclude_purchased(current_user).order(purchases_count: :desc))
+    end
 
-  def categories
-   @categories = sort_categories(Category.page(params[:page]).per(50), params[:sort_by])
-  end
+    def recent
+      @products = apply_filters_and_sort(Product.exclude_purchased(current_user))
 
-  private
+      respond_to do |format|
+        format.html
+        format.js
+      end
+    end
 
-  def filter_params
-    params.permit(:category, :language, :sort_by)
-  end
-      def sort_categories(categories, criteria)
+    def languages
+      @languages = sort_categories(Language.page(params[:page]).per(50), params[:sort_by])
+    end
+
+    def categories
+      @categories = sort_categories(Category.page(params[:page]).per(50), params[:sort_by])
+    end
+
+    private
+
+    def filter_params
+      params.permit(:category, :language, :sort_by)
+    end
+
+    def apply_filters_and_sort(resource)
+      # Apply filtering if needed (e.g., by category or language)
+      resource = resource.where(category_id: filter_params[:category]) if filter_params[:category].present?
+      resource = resource.where(language_id: filter_params[:language]) if filter_params[:language].present?
+
+      # Apply sorting based on criteria
+      case filter_params[:sort_by]
+      when 'alphabetical_asc'
+        resource.order(name: :asc)
+      when 'alphabetical_desc'
+        resource.order(name: :desc)
+      when 'oldest'
+        resource.order(created_at: :asc)
+      else
+        resource.order(name: :asc) # Default sorting
+      end.page(params[:page]).per(50)
+    end
+
+    def sort_categories(categories, criteria)
       case criteria
       when 'alphabetical_asc'
         categories.order(name: :asc)
@@ -44,5 +61,5 @@ class BrowseController < ApplicationController
         categories.order(name: :asc) # Default sorting
       end
     end
-end
+  end
 end
