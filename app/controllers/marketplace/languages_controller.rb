1804c9cc0681dbe62c21c8166825bd345f00cb58
bablu kumar <bk147411@gmail.com>
2024-09-01 09:46:30 +0530

    added pagination

--- app/controllers/marketplace/languages_controller.rb
+++ app/controllers/marketplace/languages_controller.rb
@@ -3,6 +3,7 @@ module Marketplace
 class LanguagesController < ApplicationController
   def show
     @language = Language.friendly.find(params[:slug])
+    @products = @language&.products&.page(params[:page]).per(50)
   end
 end
 end
