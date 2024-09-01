c690113e07412f5a826a7efd74cd2d723af998cb
Cody Rutscher <44309879+codyrutscher@users.noreply.github.com>
2024-08-31 21:40:46 -0500

    Update categories_controller.rb

--- app/controllers/marketplace/categories_controller.rb
+++ app/controllers/marketplace/categories_controller.rb
@@ -2,6 +2,7 @@ module Marketplace
 class CategoriesController < ApplicationController
   def show
     @category = Category.friendly.find(params[:slug])
+    @products = Product.page(params[:page]).per(50)
   end
 end
 end
