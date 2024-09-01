e5161b6c5f1630d42a3f0baa6b51341d47489663
Cody Rutscher <44309879+codyrutscher@users.noreply.github.com>
2024-08-31 21:29:43 -0500

    Update languages_controller.rb

--- app/controllers/marketplace/languages_controller.rb
+++ app/controllers/marketplace/languages_controller.rb
@@ -3,7 +3,7 @@ module Marketplace
 class LanguagesController < ApplicationController
   def show
     @language = Language.friendly.find(params[:slug])
-    @products = apply_filters_and_sort(Product.exclude_purchased(current_user)).page(params[:page]).per(50)
+    @products = Product.page(params[:page]).per(50)
   end
 end
 end
