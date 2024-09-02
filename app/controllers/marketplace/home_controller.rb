dcafcc0f20819503ee7444ca637002576ccd9ee0
Cody Rutscher <44309879+codyrutscher@users.noreply.github.com>
2024-09-02 08:39:26 -0500

    Update home_controller.rb

--- app/controllers/marketplace/home_controller.rb
+++ app/controllers/marketplace/home_controller.rb
@@ -3,7 +3,7 @@ module Marketplace
     def index
     # redirect_to dashboard_path if user_signed_in? && current_user.token.present?
     # redirect_to landing_page_path if !user_signed_in?
-
+    @product_count = Product.count 
     @languages = Language.order(:name).limit(20)
     @categories = Category.order(:name).limit(20)
     @creators = User.order(total_earning: :desc).limit(20)
