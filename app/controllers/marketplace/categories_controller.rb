7d47b5dad52f383a125a3da8b58e6fd318e20e19
Cody Rutscher <44309879+codyrutscher@users.noreply.github.com>
2024-09-02 15:49:46 -0500

    Update categories_controller.rb

--- app/controllers/marketplace/categories_controller.rb
+++ app/controllers/marketplace/categories_controller.rb
@@ -3,7 +3,6 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.friendly.find(params[:slug])
     @products = @category.products.page(params[:page]).per(50)
-    @products_count = Category.products.count
   end
 end
 end
