791c9ea97c29b2dc3a95614be27c85447e05da4e
Cody Rutscher <44309879+codyrutscher@users.noreply.github.com>
2024-10-13 19:56:23 -0500

    Revert "Development"

--- db/migrate/20241013062824_add_category_from_creator_and_language_from_creator_on_product.rb
+++
@@ -1,6 +0,0 @@
-class AddCategoryFromCreatorAndLanguageFromCreatorOnProduct < ActiveRecord::Migration[7.1]
-  def change
-    add_column :products, :more_categories_from_createor, :string, array: true, default: []
-    add_column :products, :more_languages_from_createor, :string, array: true, default: []
-  end
-end
